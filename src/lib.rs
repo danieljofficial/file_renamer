f4317ac1090de1b5a0a58cd38d472f066da7a36a
daniel <joenah004@gmail.com>
2025-06-08 22:02:13 +0100

    list_paths

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{fs::{self, read_dir}, io::{Error, ErrorKind}, path::{Path, PathBuf}};
+use std::{fs::{self}, io::{Error, ErrorKind}, path::{Path, PathBuf}};
 
 #[derive(Debug, Default)]
 pub struct ListOptions {
@@ -27,7 +27,7 @@ pub fn list_paths(path: &str, options: &ListOptions) -> Result<Vec<PathBuf>, Err
 
     let mut result = Vec::new();
 
-    list_paths_recursive(&path, options, &mut result);
+    let _ = list_paths_recursive(&path, options, &mut result);
 
     Ok(result)
 }
@@ -73,7 +73,7 @@ fn list_paths_recursive(path: &Path, options: &ListOptions, result: &mut Vec<Pat
 pub mod tests {
     use super::*;
     use std::fs::{ self, File};
-    use tempfile::{tempdir, TempDir};
+    use tempfile::{ tempdir, TempDir};
 
     fn create_test_directory() -> TempDir{
         let temp_dir = tempdir().unwrap();
@@ -114,48 +114,60 @@ pub mod tests {
         let result = list_paths(temp_dir.path().to_str().unwrap(), &options).unwrap(); 
 
         println!("Result: {:?}", result);
-        assert_eq!(result.len(), 6);
+        assert_eq!(result.len(), 5);
         assert!(result.iter().any(|p| p.ends_with("subdir")));
         assert!(result.iter().any(|p| p.ends_with("subdir/file3.txt")));
         assert!(result.iter().any(|p| p.ends_with("subdir/file4.md")));
 
     }
 
-    // #[test]
-    // fn successfully_list_directories() {
-    //     let temp_dir = tempdir().unwrap();
-    //     let dir1 = temp_dir.path().join("dir1");
-    //     let dir2 = temp_dir.path().join("dir2");
+    #[test]
+    fn test_list_paths_include_hidden() {
+        let temp_dir = create_test_directory();
+        let options = ListOptions {
+            include_hidden: true,
+            ..Default::default()
+        };
 
-    //     create_dir(&dir1).unwrap();
-    //     create_dir(&dir2).unwrap();
+        let result = list_paths(temp_dir.path().to_str().unwrap(), &options).unwrap();
 
-    //     File::create(temp_dir.path().join("file.txt")).unwrap();
+        assert!(result.iter().any(|p| p.ends_with(".hidden_dir")));
+        assert!(result.iter().any(|p| p.ends_with(".hidden_file")));
+    }
 
-    //     let result = list_directories(temp_dir.path().to_str().unwrap()).unwrap();
-    //     assert_eq!(result.len(), 2);
-    //     assert!(result.contains(&dir1));
-    //     assert!(result.contains(&dir2));
-    // }
+    #[test]
+    fn test_list_paths_filter_extensions() {
+        let temp_dir = create_test_directory();
+        let options = ListOptions {
+            extensions: Some(vec!["txt".to_string(), "md".to_string()]),
+            recursive: true,
+            ..Default::default()
+        };
 
-    // #[test]
-    // fn throws_correct_error_for_non_existent_directory() {
-    //     let result = list_directories("bad/path");
-    //     assert!(result.is_err());
-    //     let error = result.unwrap_err();
-    //     assert_eq!(error.kind(), ErrorKind::NotFound);
-    // }
-    
-    // #[test]
-    // fn throws_correct_error_for_non_directories() {
-    //     let temp_dir = tempdir().unwrap();
-    //     let file_path = temp_dir.path().join("test.txt");
+        let result = list_paths(temp_dir.path().to_str().unwrap(), &options).unwrap();
 
-    //     File::create(&file_path).unwrap();
+        assert!(result.iter().any(|p| p.ends_with("file1.txt")));
+        assert!(!result.iter().any(|p| p.ends_with("file2.rs")));
+        assert!(result.iter().any(|p| p.ends_with("subdir/file3.txt")));
+        assert!(result.iter().any(|p| p.ends_with("subdir/file4.md")));
+    }
 
-    //     let result = list_directories(file_path.to_str().unwrap());
-    //     assert!(result.is_err());
-    //     let error = result.unwrap_err();
-    //     assert_eq!(error.kind(), ErrorKind::InvalidInput);
-    // }   
+    #[test]
+    fn test_list_paths_all_filters() {
+        let temp_dir = create_test_directory();
+        let options = ListOptions {
+            recursive: true,
+            include_hidden: true,
+            extensions: Some(vec!["txt".to_string()])
+        };
+
+        let result = list_paths(temp_dir.path().to_str().unwrap(), &options).unwrap();
+
+        assert!(result.iter().any(|p| p.ends_with("file1.txt")));
+        assert!(!result.iter().any(|p| p.ends_with("file2.rs")));
+        assert!(result.iter().any(|p| p.ends_with("subdir/file3.txt")));
+        assert!(!result.iter().any(|p| p.ends_with("subdir/file4.md")));
+        assert!(result.iter().any(|p| p.ends_with(".hidden_dir")));
+    }
+
 }
